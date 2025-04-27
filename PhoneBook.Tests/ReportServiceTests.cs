5ce070c03601010351c3f62d4dc76dd0a53cb58c
Eren Mülkoğlu <mail@erenmulkoglu@gmail.com>
2025-04-27 20:54:54 +0300

    Add first unit test for ReportService (Report_Should_Set_Default_Values)

---
+++ PhoneBook.Tests/ReportServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using ReportService.Models;
+
+namespace PhoneBook.Tests
+{
+    public class ReportServiceTests
+    {
+        [Fact]
+        public void Report_Should_Set_Default_Values()
+        {
+            // Arrange
+            var report = new Report();
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, report.Id);
+            Assert.Equal(ReportStatus.Preparing, report.Status);
+            Assert.NotEqual(default(DateTime), report.RequestedAt);
+            Assert.Null(report.FilePath);
+            Assert.Null(report.Content);
+        }
+    }
+}
